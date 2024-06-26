cb7c54c1946a645ee41645c4a8d2929f3618c234
Alexis Metton <alexis.metton@gmail.com>
2024-06-26 20:04:20 +0200

    jwt bdd and logout

--- user-auth-management/auth-server.js
+++ user-auth-management/auth-server.js
@@ -6,9 +6,16 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 // Import de mysql2 afin de
 const mysql = require('mysql2/promise');
+// Chargement du fichier d'environnement
 require('dotenv').config();
+// Import du module "jsonwebtoken" pour gérer les token d'authentification et par conséquent les sessions utiilsateurs
+const jwt = require('jsonwebtoken');
+
 // Crée un serveur web avec "express"
 const app = express();
+
+// Je récupère le JWT_SECRET dans mon fichier environnement. Il permet de signer les tokens afin que le serveur puisse les valider.
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // Permet de lire et parser les données de formulaite HTML
 app.use(bodyParser.urlencoded({extended: true}))
@@ -47,7 +54,7 @@ app.use((req, res, next) => {
     }
 });
 
-// On créé une fonction qui vérifie si le contenu de la requête register contient bien un identifiant et un mot de passe
+// On créé une fonction qui vérifie si le contenu de la requête contient bien un identifiant et un mot de passe
 function validateRegisterAndLogin(req, res, next) {
     // On vérifie si au moins un des champs "identifiant" ou "mot de passe" sont absents ou vides dans le corps de la requête
     if(!req.body.identifiant || !req.body.motdepasse) {
@@ -55,6 +62,30 @@ function validateRegisterAndLogin(req, res, next) {
     }
     // On appel la fonction "next" si les champs requis sont présents afin de passer à la suite du code.
     next();
+}
+
+// On créé une fonction qui vérifie si le contenu de la requête contient bien un jeton
+function validateToken(req, res, next) {
+    // On vérifie si le champs "jeton" est absent ou vide dans le corps de la requête
+    if(!req.body.jeton) {
+        return res.status(400).json({ statut: 'Erreur', message: "JSON incorrect"})
+    }
+    // On appel la fonction "next" si le champs requis est présent afin de passer à la suite du code.
+    next();
+}
+
+// function pour générer le token d'authentification
+function generateAccessToken(user) {
+    return jwt.sign(user, JWT_SECRET, { expiresIn: '3600s' });
+}
+
+// function pour inscrire le token dans la table 'token'
+async function createToken(token, userId, db) {
+    const expiresAt = new Date(Date.now() + 3600000);
+    await db.execute(
+        'INSERT INTO tokens (token, user_id, expires_at) VALUES (?, ?, ?)',
+        [token, userId, expiresAt]
+    );
 }
 
 // On définit une route POST pour gérer les inscriptions
@@ -71,11 +102,16 @@ app.post('/register', validateRegisterAndLogin, async (req, res) => {
 
         // Je lance ce code la première fois afin de créer la table "users"
         // await req.db.execute(
-        //     'CREATE TABLE users (id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, username VARCHAR(64) NOT NULL, password BINARY(60) NOT NULL, PRIMARY KEY (id), UNIQUE(username))',
+        //     'CREATE TABLE users (id INT AUTO_INCREMENT, username VARCHAR(64) NOT NULL, password BINARY(60) NOT NULL, PRIMARY KEY (id), UNIQUE(username))',
+        // );
+
+        // Je lance ce code la première fois afin de créer la table "token" qui contiendra chaque token avec son user_id
+        // await req.db.execute(
+        //     'CREATE TABLE tokens (id INT AUTO_INCREMENT PRIMARY KEY, token TEXT NOT NULL, user_id INT NOT NULL, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, expires_at TIMESTAMP)',
         // );
 
         // On utilise 'await' afin d'attendre que la promesse soit résolue
-        await req.db.execute(
+        const [userResult] = await req.db.execute(
             // 'INSERT INTO' permet d'ajouter de nouvelles données dans une table
             // Les '?' sont des méthodes paramétrés afin d'éviter notamment l'injection de code dans un formulaire qui permet la manipulation de la base de données
             'INSERT INTO `users` VALUES(?, ?, ?)',
@@ -87,9 +123,18 @@ app.post('/register', validateRegisterAndLogin, async (req, res) => {
                 hashedpassword
             ]
         );
+        
+        // J'appelle la fonction 'generateAccessToken' afin de créer un token d'authentification
+        const accessToken = generateAccessToken({ username : username });
+
+        // Je récupère l'id de l'utilisateur qui vient d'être créé
+        const userId = userResult.insertId;
+
+        // J'appelle la fonction pour inscrire le token dans la base de données
+        createToken(accessToken, userId, req.db);
 
         // Voici ce que j'envoie si l'inscription s'est bien passé. Statut 200 pour spécifié que le serveur à réussi à traiter la requête et un message pour donner plus d'indications.
-        res.status(200).json({ statut: "Succès", message: `Utilisateur ${username} créé !` });
+        res.status(200).json({ statut: "Succès", message: `Utilisateur ${username} créé !`, token:`${accessToken}` });
     } catch (error) {
         // Voici ce que j'envoie si une erreur de duplication intervient. Statut 409 pour spécifié un conflit que le client doit résoudre. Dans notre cas, un username déjà existant.
         if (error.code === 'ER_DUP_ENTRY') {
@@ -111,7 +156,7 @@ app.post('/login', validateRegisterAndLogin, async (req, res) => {
     try {
         // Je récupère dans la base de données le password qui est lié à l'utilisateur renseigné
         const [username_exist] = await req.db.execute(
-            'SELECT password FROM `users` WHERE username = ?', [username]
+            'SELECT id, password FROM `users` WHERE username = ?', [username]
         );
 
         if(username_exist.length === 0) {
@@ -129,16 +174,44 @@ app.post('/login', validateRegisterAndLogin, async (req, res) => {
             return res.status(401).json({ "statut": "Erreur", "message": "Identifiants incorrects" });
         }
 
+        // J'appelle la fonction 'generateAccessToken' afin de créer un token d'authentification
+        const accessToken = generateAccessToken({ username: username });
+
+        // Je récupère l'id de l'utilisateur qui vient d'être créé
+        const userId = username_exist[0].id;
+
+        // J'appelle la fonction pour inscrire le token dans la base de données
+        createToken(accessToken, userId, req.db);
+
         // Voici ce que j'envoie si la connexion s'est bien passé. Statut 200 pour spécifié que le serveur à réussi à traiter la requête et un message pour donner plus d'indications.
-        res.status(200).json({ statut: "Succès", message: `Utilisateur ${username} connecté !` });
+        res.status(200).json({ statut: "Succès", message: `Utilisateur ${username} connecté !`, token: `${accessToken}` });
     } catch (error) {
         // Voici ce que j'envoie si une erreur serveur se produit. Statut 500 pour spécifié que le serveur n'est pas disponible.
         res.status(500).json({ statut: "Erreur", message: "Erreur du serveur, veuilleur réessayer ultérieurement" });
     }
 })
 
-app.get('/logout', (req, res) => {
+app.get('/logout', validateToken, async (req, res) => {
+    // Je récupère le token qui est envoyé dans le corps de la requête
+    const token = req.body.jeton;
 
+    try {
+        // Supprimer le token dans la base de données
+        const [results] = await req.db.execute(
+            'DELETE FROM tokens WHERE token = ?', [token]
+        );
+
+        // Si aucun token a été trouvé alors je renvoie une erreur 401 : Unauthorized
+        if(results.affectedRows === 0) {
+            return res.status(401).json({ statut: "Erreur", message: "Jeton inconnu" });
+        }
+
+        // Voici ce que j'envoie si la déconnexion s'est bien passé. Statut 200 pour spécifié que le serveur à réussi à supprimer le token de la base de données pour déconnecter l'utilisateur.
+        res.status(200).json({ statut: "Succès", message: `Utilisateur déconnecté !` });
+    } catch (error) {
+        // Voici ce que j'envoie si une erreur serveur se produit. Statut 500 pour spécifié que le serveur n'est pas disponible.
+        res.status(500).json({ statut: "Erreur", message: "Erreur du serveur, veuilleur réessayer ultérieurement" });
+    }
 })
 
 app.post('/verify', (req, res) => {
