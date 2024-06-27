a6cfe29f61d72b52d722697d888e20c89be9793f
Alexis Metton <alexis.metton@gmail.com>
2024-06-27 14:04:13 +0200

    login and register

---
+++ system-itineraires-v2/src/frontend/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Maps from './Maps';
+import Historique from './Historique';
+import Stations from './Stations';
+import Header from './components/Header';
+
+export default function Dashboard() {
+    return (
+        <>
+            < Header />
+        </>
+    );
+};
