da169ccecbab22040349716b9ee433ca30d6ba45
Fair Analytics <86116460+DG-Elom@users.noreply.github.com>
2024-06-28 15:43:29 +0200

    menage v2

--- system-itineraires-v2/src/_services/caller_auth.jsx
+++
@@ -1,5 +0,0 @@
-const caller_auth = {
-    API_URL: 'http://localhost:2999'
-};
-
-export default caller_auth;
--- system-itineraires-v2/src/frontend/Historique.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Historique() {
-    return (
-        <div>
-        </div>
-    );
-};
